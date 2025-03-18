53550a7d6a406af50a47460a50e080701fd5c3e6
Demilade-Afuape <122169549+X-10-Next-Gen@users.noreply.github.com>
2025-03-18 18:35:32 +0100

    Update Home.tsx

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 
 // import Header from "../Component/Header"
-import Footer from "../Component/Footer"
+// import Footer from "../Component/Footer"
 import AboutMe from "./components/AboutMe"
 import CompaniesProject from "./components/CompaniesProject"
 import HeroSection from "./components/HeroSection"
